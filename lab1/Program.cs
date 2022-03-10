52670a2a08aa5cc0f0b2316985708976c01d1417
Antilevskaya-Ksenia-153501 <92778242+Antilevskaya-Ksenia-153501@users.noreply.github.com>
2022-03-10 07:37:47 +0300

    Add files via upload

---
+++ lab1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _153501_Antilevskaya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, result;
+
+            Console.WriteLine("Enter dividend: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter divider: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            result = a / b;
+            Console.WriteLine("The result: " + result);
+        }
+    }
+}
