97e41583232ed2ed1f90458e0627ab3d0b8ac518
Antilevskaya-Ksenia-153501 <92778242+Antilevskaya-Ksenia-153501@users.noreply.github.com>
2022-03-21 17:52:33 +0300

    Add files via upload

---
+++ lab1/lab 2/task1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System; //variant 5
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, result;
+            int solution = 0;
+            do
+            {
+                Console.WriteLine("What do you want: 1) check the equation; 2) exit;");
+                while (!int.TryParse(Console.ReadLine(), out solution))
+                {
+                    Console.WriteLine("Error! Try again");
+                }
+                switch (solution)
+                {
+                    case 1:
+                    {
+                    Console.Clear();
+                    Console.WriteLine("There is equation of the form ax^2 + bc + c = 0 ");
+
+                    Console.WriteLine("Enter coefficient a: ");
+                    while (!double.TryParse(Console.ReadLine(), out a))
+                    {
+                        Console.WriteLine("Error! Try again");
+                    }
+                    
+                    Console.WriteLine("Enter coefficient b: ");
+                    while (!double.TryParse(Console.ReadLine(), out b))
+                    {
+                        Console.WriteLine("Error! Try again");
+                    }
+                    
+                    Console.WriteLine("Enter coefficient c: ");
+                    while (!double.TryParse(Console.ReadLine(), out c))
+                    {
+                        Console.WriteLine("Error! Try again");
+                    }
+                    
+                    result = b * b - 4 * a * c;
+
+                    if (result < 0)
+                        Console.WriteLine("There is no solution for such equation ");
+                    else
+                        Console.WriteLine("The equation has a solution ");
+                    break;
+                    }
+
+                    case 2:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Exit");
+                        System.Environment.Exit(0);
+                        break;
+                    }
+                    default:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Error! Try again");
+                        break;
+                    }
+                }
+            } while (solution != 2);
+        }
+    }
+}
---
+++ lab1/lab 2/task2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System; // variant 5
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            int solution = 0;
+            do
+            {
+                Console.WriteLine("What do you want: 1) check the point; 2) exit;");
+                while (!int.TryParse(Console.ReadLine(), out solution))
+                {
+                    Console.WriteLine("Error! Try again");
+                }
+                switch (solution)
+                {
+                    case 1:
+                        {
+                            Console.Clear();
+                            Console.Write("Enter point coordinates (x,y)\n x : ");
+                            while (!int.TryParse(Console.ReadLine(), out x))
+                            {
+                                Console.WriteLine("Error! Try again");
+                            }
+
+                            Console.Write("\n y : ");
+                            while (!int.TryParse(Console.ReadLine(), out y))
+                            {
+                                Console.WriteLine("Error! Try again");
+                            }
+
+                            if ((x * x + y * y < 64) && (x * x + y * y > 9) && x <= 0)
+                                Console.Write("The point lies inside the graph\n");
+                            else if (((x * x + y * y == 64) || (x * x + y * y == 9)) && x <= 0)
+                                Console.Write("The point lies on the border of the graph\n");
+                            else Console.Write("The point lies outside the graph\n");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Exit");
+                            System.Environment.Exit(0);
+                            break;
+                        }
+                    default:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Error! Try again");
+                            break;
+                        }
+                }
+            } while (solution != 2);
+        }
+    }
+}
