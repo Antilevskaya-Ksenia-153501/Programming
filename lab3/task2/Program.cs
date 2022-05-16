b6e5ffb3af1c3fbaaa03966419bfe07ec630c826
Antilevskaya-Ksenia-153501 <92778242+Antilevskaya-Ksenia-153501@users.noreply.github.com>
2022-05-16 18:46:00 +0300

    Add files via upload

---
+++ lab3/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y, z;
+            Console.WriteLine("Enter z:");
+            while (!double.TryParse(Console.ReadLine(), out  z))
+            {
+                Console.WriteLine("Error! Try again");
+            }
+
+            y = Calculations.FunctionCalculations(z);
+            Console.WriteLine($"ln(x + 0,5) + (e^x - e^-x) = {y}");
+        }
+    }
+}
