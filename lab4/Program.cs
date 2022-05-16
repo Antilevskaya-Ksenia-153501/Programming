bec1047e980617075d15b498634fda1017fc8c8d
Antilevskaya-Ksenia-153501 <92778242+Antilevskaya-Ksenia-153501@users.noreply.github.com>
2022-05-16 18:54:35 +0300

    Add files via upload

---
+++ lab4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank = Bank.getInstance("BelarusBank");
+            Console.WriteLine(bank.BankName);
+            bank.BankName = "VTB";
+            Console.WriteLine(bank.BankName);
+            
+            bank.NumberOfDeposits = 20;
+
+            bank.EditDeposit = 1000; 
+            Console.WriteLine(bank.EditDeposit);
+
+            double resultSum = bank.TotalSum();
+            Console.WriteLine(resultSum);
+
+            bank.BankName = "VTB new generation";
+            Console.WriteLine(bank.BankName);
+        }
+    }
+}
