b21f3ec8b1f563c064c2ee5db3c21ed9b9a9adaf
Antilevskaya-Ksenia-153501 <92778242+Antilevskaya-Ksenia-153501@users.noreply.github.com>
2022-05-16 18:42:39 +0300

    Add files via upload

---
+++ lab2/lab3/task1/Squaring.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    public class Squaring
+    {
+        public static uint FunctinSquaring(uint x)
+        {
+            if (x <= 0)
+            {
+                Console.WriteLine("Error. The number must be > 0");
+                return 0;
+            }
+            else
+            { 
+                return (uint)(x * x);
+            }
+        }
+    }
+}
---
+++ lab2/lab3/task3/DataServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using task3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3.Tests
+{
+    [TestClass()]
+    public class DataServiceTests
+    {
+        [TestMethod()]
+        public void GetDayTest1()
+        {
+            Assert.AreEqual("День недели: понедельник", DataService.GetDay("04.04.2022"));
+            Assert.AreEqual(1, DataService.GetDaysSpan(3, 4, 2022));
+        }
+
+        [TestMethod()]
+        public void GetDayTest2()
+        {
+            Assert.AreEqual("День недели: понедельник", DataService.GetDay("18.04.2022"));
+            Assert.AreEqual(15, DataService.GetDaysSpan(3, 4, 2022));
+        }
+
+        [TestMethod()]
+        public void GetDayTest3()
+        {
+            Assert.AreEqual("День недели: вторник", DataService.GetDay("05.04.2022"));
+            Assert.AreEqual(2, DataService.GetDaysSpan(3, 4, 2022));
+        }
+
+        [TestMethod()]
+        public void GetDayTest4()
+        {
+            Assert.AreEqual("День недели: среда", DataService.GetDay("25.05.2022"));
+            Assert.AreEqual(52, DataService.GetDaysSpan(3, 4, 2022));
+        }
+    }
+}
